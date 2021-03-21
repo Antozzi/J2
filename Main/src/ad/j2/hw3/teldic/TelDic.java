bb7a0def03e9cd6a505acdda34e270abc9c355fe
Anton Dyrdin <antondyrdin@gmail.com>
2021-03-21 17:08:50 +0500

    Home Work 3

--- Main/src/ad/j2/hw3/teldic/TelDic.java
+++ Main/src/ad/j2/hw3/teldic/TelDic.java
@@ -11,26 +11,30 @@ public class TelDic {
     }
 
     public void add(String surname, String number){
-        if(phonebook.containsKey(surname)){
-            Set<String> numbers = phonebook.get(surname);
-            if(!numbers.contains(number)){
-                numbers.add(number);
-                System.out.printf("Number %s added for %s%n", number, surname);
-            } else {
-                System.out.printf("Number %s exists for %s%n", number, surname);
-            }
-        } else {
-            phonebook.put(surname, new HashSet<>(Collections.singletonList(number)));
-            System.out.printf("Number %s added for %s%n", number, surname);
-        }
+        Set<String> numbers = phonebook.getOrDefault(surname, new HashSet<>());
+        numbers.add(number);
+        phonebook.putIfAbsent(surname,numbers);
+//        if(phonebook.containsKey(surname)){
+//            Set<String> numbers = phonebook.get(surname);
+//            if(!numbers.contains(number)){
+//                numbers.add(number);
+//                System.out.printf("Number %s added for %s%n", number, surname);
+//            } else {
+//                System.out.printf("Number %s exists for %s%n", number, surname);
+//            }
+//        } else {
+//            phonebook.put(surname, new HashSet<>(Collections.singletonList(number)));
+//            System.out.printf("Number %s added for %s%n", number, surname);
+//        }
     }
 
     public Set<String> get(String surname){
-        if(phonebook.containsKey(surname)){
-            return phonebook.get(surname);
-        } else {
-            System.out.printf("There is no record of %s in phonebook%n", surname);
-            return new HashSet<>();
-        }
+        return phonebook.getOrDefault(surname, Collections.emptySet());
+//        if(phonebook.containsKey(surname)){
+//            return phonebook.get(surname);
+//        } else {
+//            System.out.printf("There is no record of %s in phonebook%n", surname);
+//            return new HashSet<>();
+//        }
     }
 }
