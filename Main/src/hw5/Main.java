00d23668ae2b0fb56b8dfa7e22e8c0d3d4ba20e3
Anton Dyrdin <antondyrdin@gmail.com>
2021-03-29 14:22:42 +0500

    Home Work 5 - update after hw overseeing

--- Main/src/hw5/Main.java
+++ Main/src/hw5/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private static final int size = 10000000;
     private static final int h = size / 2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Main mainClass = new Main();
         mainClass.methodOne();
         mainClass.methodTwo();
@@ -24,7 +24,7 @@ public class Main {
         System.out.println(String.format("Method One accomplished. Exec time: %s", (end - start)));
     }
 
-    private void methodTwo() {
+    private void methodTwo() throws InterruptedException {
         System.out.println("Method Two launched");
         float[] arr = new float[size];
         float[] arr1 = new float[h];
@@ -36,12 +36,20 @@ public class Main {
         long split = System.currentTimeMillis();
         System.out.println(String.format("Array split time %s", (split - start)));
 
-        Thread thread1 = new Thread(() -> methodTwoThreaded(arr1, 1));
-        Thread thread2 = new Thread(() -> methodTwoThreaded(arr2, 2));
-        thread1.start();
-        thread2.start();
-
+        //Start synced threads
         long connect = System.currentTimeMillis();
+        Thread t1 = new Thread(() -> {
+            methodTwoThreaded(arr1, 1);
+        });
+        Thread t2 = new Thread(() -> {
+            methodTwoThreaded(arr2, 2);
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        //End synced threads
+
         System.arraycopy(arr1, 0, arr, 0, h);
         System.arraycopy(arr2, 0, arr, h, h);
         long end = System.currentTimeMillis();
